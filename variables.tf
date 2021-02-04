f2a83825444d3056aaf1189507d7c8c8896f7133
tom <treilly94@live.com>
2021-02-04 17:05:10 +0000

    initial commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "env" {
+  default = "dev"
+}
+
+variable "cloudflare_token" {
+  default = ""
+}
+
+variable "acme_url" {
+  default = "https://acme-staging-v02.api.letsencrypt.org/directory"
+}
